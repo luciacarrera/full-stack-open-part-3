4c49e77f170e01a9b30f8dcdb919ae10b23f1984
Lucia Carrera <luciaacarreraa@gmail.com>
2025-06-23 19:45:03 +0200

    uibuild

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,8 +15,11 @@ export default [
     rules: {
       '@stylistic/js/indent': ['error', 2],
       eqeqeq: 'error',
-      'no-trailing-spaces': 'error', 'object-curly-spacing': ['error', 'always'], 'arrow-spacing': ['error', { before: true, after: true }],
-      '@stylistic/js/linebreak-style': ['error', 'unix'], '@stylistic/js/quotes': ['error', 'single'], '@stylistic/js/semi': ['error', 'never'], 'no-console': 'off',
+      'no-trailing-spaces': 'error', 
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      '@stylistic/js/linebreak-style': ['error', 'unix'], 
+      '@stylistic/js/quotes': ['error', 'single'], '@stylistic/js/semi': ['error', 'never'], 'no-console': 'off',
     },
   }, { ignores: ['dist/**'], },
 ]
